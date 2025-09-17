0976de538ad64190d5a2c0a721e3ddba4410b5d7
Pranav Mishra <89926012+PranavMishra17@users.noreply.github.com>
2025-09-17 01:10:03 -0400

    run

---
+++ run.bat
@@ -0,0 +1,27 @@
+python slm_runner.py --dataset medmcqa --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset medmcqa --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset medmcqa --method cot --model gemma3_4b --num_questions 20
+
+python slm_runner.py --dataset mmlupro-med --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset mmlupro-med --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset mmlupro-med --method cot --model gemma3_4b --num_questions 20
+
+python slm_runner.py --dataset pubmedqa --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset pubmedqa --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset pubmedqa --method cot --model gemma3_4b --num_questions 20
+
+python slm_runner.py --dataset ddxplus --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset ddxplus --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset ddxplus --method cot --model gemma3_4b --num_questions 20
+
+python slm_runner.py --dataset medbullets --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset medbullets --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset medbullets --method cot --model gemma3_4b --num_questions 20
+
+python slm_runner.py --dataset pmc_vqa --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset pmc_vqa --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset pmc_vqa --method cot --model gemma3_4b --num_questions 20
+
+python slm_runner.py --dataset path_vqa --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset path_vqa --method cot --model gemma3_4b --num_questions 20
+python slm_runner.py --dataset path_vqa --method cot --model gemma3_4b --num_questions 20
